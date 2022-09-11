e6bc03adcfe154344881deda0d8288e62cb6363d
川島慎悟 <kawashingo1990@gmail.com>
2022-09-11 22:25:08 +0900

    ジェネレータを利用した非同期プログラミング

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function* tryCatchGeneratorFunc() {
+function parseJSONAsync(json) {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+        resolve(JSON.parse(json))
+      } catch (err) {
+        reject(err)
+      }
+    }, 1000)
+  )
+}
+
+function* asyncWithGeneratorFunc(json) {
   try {
-    yield 1
+    const result = yield parseJSONAsync(json)
+    console.log('パース結果', result)
   } catch (err) {
     console.log('エラーをキャッチ', err)
-    yield 2
   }
 }
 
-const tryCatchGenerator = tryCatchGeneratorFunc()
-console.log(tryCatchGenerator.next())
-console.log(tryCatchGenerator.throw(new Error('エラー')))
-console.log(tryCatchGenerator.next())
+const asyncWithGenerator1 = asyncWithGeneratorFunc('{"foo":1}')
+const promise1 = asyncWithGenerator1.next().value
+console.log(promise1)
+promise1.then((result) => {
+  asyncWithGenerator1.next(result)
+})
