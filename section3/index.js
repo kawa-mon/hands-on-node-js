af04863fb3d6baab1ec9ae789c40704cfe0285ed
川島慎悟 <kawashingo1990@gmail.com>
2022-09-17 21:48:46 +0900

    EventEmitterのPromise化

--- section3/index.js
+++ section3/index.js
@@ -1,20 +1,7 @@
 const events = require('events')
 
-const eventAEmitter = new events.EventEmitter()
-const eventAIterable = events.on(eventAEmitter, 'eventA')
-console.log(eventAEmitter.listeners('eventA'))
-;(async () => {
-  for await (const a of eventAIterable) {
-    if (a[0] === 'end') {
-      break
-    }
-    console.log('eventA', a)
-  }
-})()
-
-eventAEmitter.emit('eventA', 'Hello')
-eventAEmitter.emit('eventA', 'Hello', 'World')
-eventAEmitter.emit('eventA', 'end')
-setTimeout(() => {
-  console.log(eventAEmitter.listeners('eventA'))
-}, 100)
+const eventBEmitter = new events.EventEmitter()
+const eventBPromise = events.once(eventBEmitter, 'eventB')
+eventBPromise.then((arg) => console.log('eventB発生', arg))
+eventBEmitter.emit('eventB', 'Hello', 'World')
+eventBEmitter.emit('eventB', 'one more')
