7573d3a7810521cff7010706e57fd1a143f8bb25
川島慎悟 <kawashingo1990@gmail.com>
2022-09-19 06:08:56 +0900

    継承を用いた自前の読み込みストリーム

--- section3/index.js
+++ section3/index.js
@@ -1,14 +1,32 @@
-const fs = require('fs')
+const stream = require('stream')
 
-const readStream = fs.createReadStream('section3/src.txt')
-readStream
+class HelloReadableStream extends stream.Readable {
+  constructor(options) {
+    super(options)
+    this.languages = ['JavaScript', 'Python', 'Java', 'C#']
+  }
+
+  _read(size) {
+    console.log('_read()')
+    let language
+    while ((language = this.languages.shift())) {
+      if (!this.push(`Hello, ${language}!\n`)) {
+        console.log('読み込み中断')
+        return
+      }
+    }
+    console.log('読み込み完了')
+    this.push(null)
+  }
+}
+
+const helloReadableStream = new HelloReadableStream()
+helloReadableStream
   .on('readable', () => {
     console.log('readable')
     let chunk
-    while ((chunk = readStream.read()) !== null) {
+    while ((chunk = helloReadableStream.read()) !== null) {
       console.log(`chunk: ${chunk.toString()}`)
     }
   })
-  .on('end', () => {
-    console.log('end')
-  })
+  .on('end', () => console.log('end'))
