8cadf0a5467c0f3e0856f6b123f7bc8a3ef285df
川島慎悟 <kawashingo1990@gmail.com>
2022-10-01 06:49:12 +0900

    POSTリクエストのハンドリング

--- todo-express/app.js
+++ todo-express/app.js
@@ -5,6 +5,7 @@ let todos = [
   { id: 2, title: '下書き', completed: true },
 ]
 const app = express()
+app.use(express.json())
 
 app.get('/api/todos', (req, res) => {
   if (!req.query.completed) {
@@ -12,6 +13,24 @@ app.get('/api/todos', (req, res) => {
   }
   const completed = req.query.completed === 'true'
   res.json(todos.filter((todo) => todo.completed === completed))
+})
+
+let id = 2
+app.post('/api/todos', (req, res, next) => {
+  const { title } = req.body
+  if (typeof title !== 'string' || !title) {
+    const err = new Error('title is required')
+    err.statusCode = 400
+    return next(err)
+  }
+  const todo = { id: (id += 1), title, completed: false }
+  todos.push(todo)
+  res.status(201).json(todos)
+})
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.statusCode || 500).json({ error: err.message })
 })
 
 app.listen(3000)
