e38db71719abd365e337e19a989f670d9a4dceec
川島慎悟 <kawashingo1990@gmail.com>
2022-10-01 20:40:55 +0900

    章末問題

--- todo-express/app.js
+++ todo-express/app.js
@@ -28,6 +28,34 @@ app.post('/api/todos', (req, res, next) => {
   res.status(201).json(todo)
 })
 
+app.use('/api/todos/:id(\\d+)', (req, res, next) => {
+  const targetId = Number(req.params.id)
+  const todo = todos.find((todo) => todo.id === targetId)
+  if (!todo) {
+    const err = new Error('ToDo not found')
+    err.statusCode = 404
+    return next(err)
+  }
+  req.todo = todo
+  next()
+})
+
+app
+  .route('/api/todos/:id(\\d+)/completed')
+  .put((req, res) => {
+    req.todo.completed = true
+    res.json(req.todo)
+  })
+  .delete((req, res) => {
+    req.todo.completed = false
+    res.json(req.todo)
+  })
+
+app.delete('/api/todos/:id(\\d+)', (req, res) => {
+  todos = todos.filter((todo) => todo !== req.todo)
+  res.status(204).end()
+})
+
 app.use((err, req, res, next) => {
   console.error(err)
   res.status(err.statusCode || 500).json({ error: err.message })
